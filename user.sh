eed307b0173f6d4d640f21b992a263fc0c92bbfc
Brian Foster <colakatz>
2021-02-07 11:25:28 -0500

    Adding ark server tools to path

--- user.sh
+++ user.sh
@@ -11,6 +11,10 @@ if [ ! "$(id -g steam)" -eq "$GID" ]; then
 	groupmod -o -g "$GID" steam ;
 fi
 
+# Add to Path
+PATH=$PATH:/home/steam/ark-server-tools/:/home/steam/ark-server-tools/tools/
+
+
 # Put steam owner of directories (if the uid changed, then it's needed)
 chown -R steam:steam /ark /home/steam
 
